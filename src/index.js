c70fd7aca973535af972dd1769aaf65be7b6777d
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2025-11-10 04:43:40 +0800

    Variables for the slider or carousel

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 /* Image Carousel 01 - index.js */
 import "./style.css";
+
+const imgCnt = document.getElementById("imageContainer");
+
+const imgArray = document.getElementsByTagName("img");
+
+let currentIndex = 0;
+
+let timeInterval = 5000;
